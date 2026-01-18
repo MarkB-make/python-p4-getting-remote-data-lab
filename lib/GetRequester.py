e514f502fd30215c399b313ef8f927c507f2d079
Mark <jadebrian999@gmail.com>
2026-01-18 21:46:31 +0300

    GetRequester

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,16 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
+        """Initialize the GetRequester with a URL string."""
         self.url = url
-
+    
     def get_response_body(self):
-        pass
-
+        """Send a GET request to the URL and return the response body."""
+        response = requests.get(self.url)
+        return response.content
+    
     def load_json(self):
-        pass
+        """Send a request and return the JSON data as a Python list or dictionary."""
+        response_body = self.get_response_body()
+        return json.loads(response_body)
